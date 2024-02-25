82b514d5f5a37aed8ef4c0ae92c8272c98240dff
Derek Peacock <derek.peacock@yahoo.com>
2024-02-25 10:54:51 +0000

    Finally got modules working

--- FSU_ConsoleApp/Program.fs
+++ FSU_ConsoleApp/Program.fs
@@ -1,8 +1,9 @@
-﻿
+﻿module Program
+
 [<EntryPoint>]
 let main argv =
-    
-    let example = Week01.exercise03
-    0 // Exit code
+    let example = Week01.exercise03.run()
+
+    0 // return an integer exit code
 
 
--- FSU_ConsoleApp/week01.fs
+++ FSU_ConsoleApp/week01.fs
@@ -2,31 +2,33 @@
 
 open System
 
-// Exercise 01
+    // Exercise 01
 
-let exercise01 = 
+module exercise01 = 
     
-    let name = "Kate Ibom"
-    let age = 21
-    let height = 1.8
+    let run () = 
+        let name = "Kate Ibom"
+        let age = 21
+        let height = 1.8
     
-    printfn "%s" name
-    printfn "%i" age
-    printfn "%f" height
+        printfn "%s" name
+        printfn "%i" age
+        printfn "%f" height
 
 // Exercise 3
 
-let exercise03 =
+module exercise03 =
 
-    printfn "Enter your name"
-    let name = Console.ReadLine()
+    let run () =
+        printfn "Enter your name"
+        let name = Console.ReadLine()
 
-    let age = Helpers.getInteger "Enter your age > " 0 120
+        let age = Helpers.getInteger "Enter your age > " 0 120
 
-    Console.Write("Enter your height in meters > ")
-    let height = float(Console.ReadLine())
+        Console.Write("Enter your height in meters > ")
+        let height = float(Console.ReadLine())
 
-    Console.WriteLine($"Your name = {name}")
-    Console.WriteLine($"Your age is {age} years")
-    Console.WriteLine($"Your height is {height} meters")
+        Console.WriteLine($"Your name = {name}")
+        Console.WriteLine($"Your age is {age} years")
+        Console.WriteLine($"Your height is {height} meters")
 
